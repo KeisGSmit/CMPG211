f130efd59118eccba840c1669970e3938ed2704b
Keis Smit <kgsmit98@gmail.com>
2024-03-14 21:16:19 +0200

    Start of week 4

---
+++ 20240308/week3.java
@@ -0,0 +1,16 @@
+public class week3 {
+
+    // Attributes describe objects
+
+    // Constructors used to create objects
+
+    // Accessors - "Get" attribute values (method)
+
+    // Mutators - "Set" attribute values (method)
+
+    
+    public static void main(String[] args){
+        
+    }
+    
+}
