459fe681b5860ec51070db7690b822f73b4ded41
Keis Smit <kgsmit98@gmail.com>
2024-03-15 23:11:53 +0200

    Polymorphism in inheritance

--- 20240314/week4_testPerson.java
+++ 20240314/week4_testPerson.java
@@ -1,7 +1,8 @@
 public class week4_testPerson {
     public static void main(String[] args){
     // Need to account for new super class
-    // Test person constructor and methods
+    //Test person constructor and methods
+
         week4_person p1 = new week4_person("25648997", "Person", "person@email.com");
         System.out.println(p1);
 
@@ -33,6 +34,20 @@ public class week4_testPerson {
             "Prof");
         
         System.out.println(st1);
+    
+    // Polymorphism
+
+        week4_person s2 = new week4_student(); // student extends from person
+        week4_person f2 = new week4_faculty(); // faculty inhertis from person class
+        week4_person st2 = new week4_staff(); // staff inherits from person class
+
+        System.out.println(s2); // why? 
+            // 1) student inherits from person class 
+            // 2) objects from different classes can be regarded as objects from a super class
+        // can only call methods that person has, not student / faculty / staff
+        System.out.println(f2);
+        System.out.println(st2);
+
     } 
     
 }
