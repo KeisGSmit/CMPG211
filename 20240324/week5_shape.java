1436b978beef48ea18ca2583c2a7046422c385bb
Keis Smit <kgsmit98@gmail.com>
2024-03-24 13:12:35 +0200

    Creating shape.java content

--- 20240324/week5_shape.java
+++ 20240324/week5_shape.java
@@ -1,3 +1,24 @@
 public class week5_shape {
-    
+    private float dimension;
+
+    week5_shape(){
+        this((float) Math.random() * 10);
+    }
+
+    week5_shape(float dimension){
+        this.setDimension(dimension);
+    }
+
+    public float getDimention(){
+        return this.dimension;
+    }
+
+    public void setDimension(float dimension) {
+        this.dimension = dimension;
+    }
+
+    public String toString(){
+        return super.toString();
+    }
+
 }
