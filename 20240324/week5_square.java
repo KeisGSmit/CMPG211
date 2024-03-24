09fc71fa9f1ef3d15d65ad39ade5ec18d264d2f3
Keis Smit <kgsmit98@gmail.com>
2024-03-24 15:12:18 +0200

    Polymorphism in arrays

--- 20240324/week5_square.java
+++ 20240324/week5_square.java
@@ -16,11 +16,23 @@ public class week5_square extends week5_shape{
 
     public static void main(String[] args){
 
-        // Use of inheritance
-        week5_square S1 = new week5_square(20);
-        System.out.println(S1.getPermimeter());
+        // // Use of inheritance
+        // week5_square S1 = new week5_square(20);
+        // System.out.println(S1.getPermimeter());
 
-        week5_circle circ1 = new week5_circle(20);
-        System.out.println(circ1.getPermimeter());
+        // week5_circle circ1 = new week5_circle(20);
+        // System.out.println(circ1.getPermimeter());
+
+        // Wild idea -> polymorphism -> reads square as a shape not as a square
+        week5_shape shape1 = new week5_square(10);
+        System.out.println(shape1);
+
+        // ^ This is useful when it comes to arrays
+
+        week5_shape[] arrShapes = {new week5_square(10), new week5_circle(20)};
+
+        for(int i= 0; i < arrShapes.length ; i ++ ){
+            System.err.println(arrShapes[i]);
+        }
     }
 }
