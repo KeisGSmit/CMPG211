18cc09e2931a84d577d28ce96c40e7e32c3ca625
Keis Smit <kgsmit98@gmail.com>
2024-03-24 18:34:56 +0200

    Demonstarating the power of interfaces

--- 20240324/week5_person.java
+++ 20240324/week5_person.java
@@ -1,3 +1,50 @@
 public class week5_person{
-    
+    private String name;
+    private int age;
+
+    public week5_person(String name, int age){
+        this.setAge(age);
+        this.setName(name);
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public void setName(String name){
+        this.name = name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setAge(int age){
+        this.age = age;
+    }
+
+    public String getAgeString(){
+        return "Age: " + age;
+    }
+
+    @Override
+    public String toString(){
+        return "Person{" +
+                "Name='" + name + '\'' +
+                " age=" + age +
+                '}';
+    }
+
+    public static void main(String[] args){
+        week5_person[] persons = new week5_person[5];
+        persons[0] = new week5_person("Doe", 35);
+        persons[1] = new week5_person("John", 25);
+        persons[2] = new week5_person("Doe", 45);
+        persons[3] = new week5_person("Jane", 30);
+        persons[4] = new week5_person("Smith", 40);
+
+        for(week5_person person: persons){
+            System.err.println(person.getAgeString());
+        }
+    }
 }
