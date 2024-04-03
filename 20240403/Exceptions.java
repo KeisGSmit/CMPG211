6143d8961e942317dbafdff332be6bdddef283cf
Keis Smit <kgsmit98@gmail.com>
2024-04-03 18:50:52 +0200

    big fix

--- 20240403/Exceptions.java
+++ 20240403/Exceptions.java
@@ -3,19 +3,18 @@ import java.util.Scanner;
 public class Exceptions {
     
     public static void main(String[] args){
-
+        Scanner scanner;
         try{
-            Scanner scanner = new Scanner(System.in);
+            scanner = new Scanner(System.in);
 
             System.out.println("Enter a integer: ");
             int num = Integer.parseInt(scanner.nextLine());
 
-            scanner.close(); // Add this KEIS!!!!!
 
             System.err.println("The number you entered: " + num);
         
-
-            // Trickle down Exception catching
+            scanner.close();
+            // Trickle down Exceptioncatching
         }catch(NumberFormatException e){
             System.err.println("There was an error: " + e);
         }catch(Exception ex){
@@ -23,8 +22,25 @@ public class Exceptions {
 
         }finally{       // Executes after exceptions were thrown
 
+            // You can close db connections or scanners here
             System.err.println("The code is done executing");
         }
         
+
+        try {
+            scanner = new Scanner(System.in);
+
+            System.err.println("Enter a divisor: ");
+            int divisor = Integer.parseInt(scanner.nextLine());
+            int results = 1/divisor;
+
+            System.err.println("This is the result: " + results);
+
+            scanner.close();
+            
+        } catch (ArithmeticException e) {
+            System.err.println("go back to Grade 1: " + e);
+        }finally{
+        }
     }
 }
